6def5bafcd67aaa9a8c79f1f81de14ee49c74574
Manish <manish.dalal@moonshinetechnology.com>
2023-05-08 09:44:17 +0530

    chanddd

---
+++ bash.sh
@@ -0,0 +1,7 @@
+echo "starting Bot ~@DroneBots";
+echo $PORT
+kill -9 $(lsof -t -i:$PORT)
+nohup python3 webApp.py > save_pid.txt >&1 &
+# lsof -t -i:8082   
+# kill -9 $(lsof -t -i:8082)
+python3 -m bot
